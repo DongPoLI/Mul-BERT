62b12b76686a9d720feded568dbe1232490c9653
Shenzhen Li <1185943734@qq.com>
2021-01-09 16:39:06 +0800

    mul_bert_b model file

---
+++ mul_bert_b/config.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+@Time   : 2021/1/8 下午8:08
+@Author : Li Shenzhen
+@File   : config.py
+@Software:PyCharm
+"""
+
+
+class Config:
+    # pretrained_weights = "bert-base-uncased"
+    pretrained_weights = "bert-large-uncased"
+
+    # 模型路径
+    # checkpoint_dir = "mul_bert_b_model/mul_bert_b_large_9064_checkpoint.pth.tar"
+    checkpoint_dir = "mul_bert_b_model/mul_bert_b_large_9072_checkpoint.pth.tar"
+
+
+config = Config()
